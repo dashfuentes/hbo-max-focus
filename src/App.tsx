820252f6ce921df3870a6eb2b92b1d3fd188dd87
dashfuentes <smis117112@gmail.com>
2022-10-24 16:10:26 -0600

    feat: add the route to the Router context

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css'
 import Login from './components/screens/Login/index';
 import Home from './components/screens/Home';
+import Favorite from './components/screens/Favorites';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/home" element={<Home />} />
+        <Route exact path="/favorite-movie" element={<Favorite />} />
         </Routes>
       
     </Router>
