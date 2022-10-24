a6a3899d2f483ce67c7b877d1433a4819717d2f3
dashfuentes <smis117112@gmail.com>
2022-10-24 17:23:53 -0600

    feat: verify home session and adjust the template

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -1,48 +1,42 @@
 import React, { FC, useState, useEffect } from "react";
 import { getFeaturedMovies } from "../../../utils";
+import { verifySessionToken } from "../../../utils";
 import MovieList from "../../atoms/MovieList";
-import Navbar from "../../atoms/Navbar"
+import Navbar from "../../atoms/Navbar";
 import useStore from "../../../config/globalStore";
 import { useNavigate } from "react-router-dom";
+
 
 interface Props {}
 
 const Home: FC<Props> = (props) => {
-	const navigate = useNavigate();
 	const [featuredMovies, setFeaturedMovies] = useState<any[]>([]);
-	//const [isLoggedin, setIsLoggedin] = useState<Boolean>(false);
+	const navigate = useNavigate()
 	const getToken = useStore((state: any) => state.token);
-	console.log('token from home', getToken.token)
-	
-	//make utils fn
-	const verifySessionToken = (tokenObj: any) => {
-		console.log('token from verify', tokenObj)
-		if (tokenObj.token == "") return  navigate("/");
-		
-		
-	}
+	const getTokenSession = localStorage.getItem("token");
+	//console.log("token from home", getTokenSession);
+
 	useEffect(() => {
-		verifySessionToken(getToken)
+		const checkSession = verifySessionToken(getToken, getTokenSession);
+		if(!checkSession) return navigate('/')
 		getFeaturedMovies()
 			.then((features) => {
-				console.log("movies", features.results);
-				setFeaturedMovies(features.results);
+				console.log("movies", features);
+				setFeaturedMovies(features);
 			})
 			.catch((e) => console.log(e));
-	}, [getToken]);
+	}, [getTokenSession]);
 	return (
 		<>
-		<Navbar />
-			<div className="container mx-auto"  >
-			<div className="flex flex-wrap -mx-4">
-			
-				{featuredMovies.map((movie) => (
-					<MovieList key={movie.id} movie={movie} />
-				))}
-					</div>
-			
+			<Navbar />
+			<div className="container mx-auto">
+				<div className="flex flex-wrap -mx-4">
+					{featuredMovies.map((movie) => (
+						<MovieList key={movie.id} movie={movie} />
+					))}
+				</div>
 			</div>
-			</>
+		</>
 	);
 };
 
