672d44d277e1312625d5ccce3516e75be226b4ab
dashfuentes <smis117112@gmail.com>
2022-10-24 16:25:51 -0600

    feat: Add search input component to the template

--- src/components/atoms/Navbar/index.tsx
+++ src/components/atoms/Navbar/index.tsx
@@ -2,24 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo-hbo-max.jpg";
 import SearchInput from "../../atoms/SearchInput";
-import Logout from "../../atoms/Logout"
+import { body } from "../../../config/globalStyles";
 
 const Navbar = () => {
 	return (
 		<>
-			<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-				<div className="container flex flex-wrap justify-between items-center mx-auto">
-					<a href="/" className="flex items-center">
-						<img src={logo} className="mr-3 h-6 sm:h-10" alt="hbo-max-logo" />
-						<span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-							HBO MAX
-						</span>
+			<nav className="" style={body}>
+				<div className="flex flex-wrap justify-between items-center mx-auto container">
+					<a href="#" className="flex items-center">
+						<img src={logo} className="mr-3 h-8 sm:h-10" alt="logo" />
 					</a>
 					<button
-						data-collapse-toggle="navbar-default"
+						data-collapse-toggle="mobile-menu"
 						type="button"
-						className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-						aria-controls="navbar-default"
+						className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
+						aria-controls="mobile-menu-2"
 						aria-expanded="false"
 					>
 						<span className="sr-only">Open main menu</span>
@@ -37,17 +34,30 @@ const Navbar = () => {
 							></path>
 						</svg>
 					</button>
-					<div className="hidden w-full md:block md:w-auto" id="navbar-default">
-						<ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+					<div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+						<ul className="flex flex-col p-4 mt-4  rounded-lg border  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 							<li>
-								<Link className="nav-link" to="/home">
+								<Link
+									className="nav-link block py-2 pr-4 pl-3 0 rounded md:bg-transparent  md:p-0  text-xl pb-4 pt-2 font-medium text-white"
+									to="/home"
+								>
 									Home
 								</Link>
 							</li>
+							<li>
+								<Link
+									className="nav-link block py-2 pr-4 pl-3 0 rounded md:bg-transparent     pt-2 text-xl text-white"
+									to="/favorite-movie"
+								>
+									Favorites
+								</Link>
+							</li>
+
+							<li>
+								<SearchInput />
+							</li>
 						</ul>
 					</div>
-					<SearchInput />
-					<Logout />
 				</div>
 			</nav>
 		</>
