4ca034f5a51a3fdd74105bb77567e302998a3bab
dashfuentes <smis117112@gmail.com>
2022-10-24 16:31:30 -0600

    remove token from LS

--- src/components/atoms/Logout/index.tsx
+++ src/components/atoms/Logout/index.tsx
@@ -8,11 +8,18 @@ const Logout: FC<Props> = (props) => {
 
     const deleteToken = () => {
         console.log('removing')
+        
+        localStorage.removeItem('token');
+        //remove the favorites from LS
+
+
         return removeTokenFromStore()
     }
 	
-	return (
-		<button type="button" onClick={ () => {return deleteToken()}} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Logout</button>
+    return (
+       
+            <button type="button" onClick={() => { return deleteToken() }} className="text-white  right-2.5 bottom-2.5 bg-red-700 hover:bg-red-799 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Logout</button>
+           
 	);
 };
 
