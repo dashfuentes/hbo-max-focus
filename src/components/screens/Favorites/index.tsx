8066359576b6a04c759ad329fb372c5d4285fdd9
dashfuentes <smis117112@gmail.com>
2022-10-24 16:09:28 -0600

    feat:create a store fn in order to render them

---
+++ src/components/screens/Favorites/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useEffect } from "react";
+import ListItem from "../../atoms/MovieList";
+import Navbar from "../../atoms/Navbar";
+import { verifySessionToken } from "../../../utils";
+import { useNavigate } from "react-router-dom";
+import useStore from "../../../config/globalStore";
+
+interface Props {}
+
+const Favorite: FC<Props> = (props) => {
+	const navigate = useNavigate()
+	const getToken = useStore((state: any) => state.token);
+	const getTokenSession = localStorage.getItem("token");
+	const favoriteMovies = useStore((state: any) => state.favoriteMovies);
+	useEffect(() => {
+		const checkSession = verifySessionToken(getToken, getTokenSession);
+		if(!checkSession) return navigate('/')
+		
+	}, [getTokenSession]);
+	return (
+		<>
+			<Navbar />
+			<div className="container mx-auto"  >
+			<div className="flex flex-wrap -mx-4">
+					{favoriteMovies.map((movie: any) => (
+					<ListItem key={movie.id} movie={movie} />
+				))}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Favorite;
--- src/config/globalStore.tsx
+++ src/config/globalStore.tsx
@@ -6,15 +6,27 @@ const useStore = create((set) => ({
 	addToken: (key: any) =>
 		set((state: any) => ({
 			token: { ...state.token, token: key.token },
-			
 		})),
 
-		removeToken: () =>
+	removeToken: () =>
 		set((state: any) => ({
-			token: {  ...state.token, token: "" },
-			
+			token: { ...state.token, token: "" },
 		})),
 
-	
+	favoriteMovies: [],
+	addMovies: (movie: any) =>
+		set((state: any) => ({
+			favoriteMovies: [
+				{
+					id: movie.id,
+					title: movie.title,
+					poster_path: movie.poster_path,
+					release_date: movie.release_date,
+					popularity: movie.popularity,
+					overview: movie.overview,
+				},
+				...state.favoriteMovies,
+			],
+		})),
 }));
 export default useStore;
