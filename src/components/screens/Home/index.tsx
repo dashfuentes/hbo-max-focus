154ed8e7d37f207026fc88e89ac8a48dd1414463
dashfuentes <smis117112@gmail.com>
2022-10-25 13:07:50 -0600

    refact: cache the get movies fn with react/query

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -5,12 +5,15 @@ import MovieList from "../../atoms/MovieList";
 import Navbar from "../../atoms/Navbar";
 import useStore from "../../../config/globalStore";
 import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
+import Loading from '../../atoms/Loading'
 
 
 interface Props {}
 
 const Home: FC<Props> = (props) => {
-	const [featuredMovies, setFeaturedMovies] = useState<any[]>([]);
+
+	//const [featuredMovies, setFeaturedMovies] = useState<any[]>([]);
 	const navigate = useNavigate()
 	const getToken = useStore((state: any) => state.token);
 	const getTokenSession = localStorage.getItem("token");
@@ -19,19 +22,18 @@ const Home: FC<Props> = (props) => {
 	useEffect(() => {
 		const checkSession = verifySessionToken(getToken, getTokenSession);
 		if(!checkSession) return navigate('/')
-		getFeaturedMovies()
-			.then((features) => {
-				console.log("movies", features);
-				setFeaturedMovies(features);
-			})
-			.catch((e) => console.log(e));
 	}, [getTokenSession]);
+	const { isLoading, isError, data, error } = useQuery(['movies'], getFeaturedMovies)
+
+	if (isLoading) {
+		return <Loading />;
+	  }
 	return (
 		<>
 			<Navbar />
 			<div className="container mx-auto">
 				<div className="flex flex-wrap -mx-4">
-					{featuredMovies.map((movie) => (
+					{data && data.map((movie:any) => (
 						<MovieList key={movie.id} movie={movie} />
 					))}
 				</div>
