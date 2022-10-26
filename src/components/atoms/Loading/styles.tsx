69f1745ef997f634d70c7f7ab52a1c15fdb56b20
dashfuentes <smis117112@gmail.com>
2022-10-26 10:13:47 -0600

    Updates

--- src/components/atoms/Loading/styles.tsx
+++
@@ -1,9 +0,0 @@
-import CSS from 'csstype';
-
-export const lottie: CSS.Properties = {
-    width: '100vw',
-    height: '100vh',
-    position: 'fixed',
-    margin: 'auto',
-    marginTop: '-86px',
-};
--- src/config/globalStyles.tsx
+++
@@ -1,5 +0,0 @@
-import CSS from 'csstype';
-
-export const body: CSS.Properties = {
-    background: 'rgb(46 4 55 / 55%)'
-};
