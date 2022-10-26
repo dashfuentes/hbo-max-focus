1bb7c7eea15d8d12640a5c764dfe278ca21a7a33
dashfuentes <smis117112@gmail.com>
2022-10-26 06:57:35 -0600

    fix: adjust display animation conditional render

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -5,37 +5,45 @@ import MovieList from "../../atoms/MovieList";
 import Navbar from "../../atoms/Navbar";
 import useStore from "../../../config/globalStore";
 import { useNavigate } from "react-router-dom";
-import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
-import Loading from '../../atoms/Loading'
-
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../atoms/Loading";
 
 interface Props {}
 
 const Home: FC<Props> = (props) => {
-
-	//const [featuredMovies, setFeaturedMovies] = useState<any[]>([]);
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const getToken = useStore((state: any) => state.token);
+	const [showAnimation, setShowAnimation] = useState<any>(false);
 	const getTokenSession = localStorage.getItem("token");
-	//console.log("token from home", getTokenSession);
+	const favoriteMovies = useStore((state: any) => state.favoriteMovies);
 
 	useEffect(() => {
 		const checkSession = verifySessionToken(getToken, getTokenSession);
-		if(!checkSession) return navigate('/')
+		if (!checkSession) return navigate("/");
 	}, [getTokenSession]);
-	const { isLoading, isError, data, error } = useQuery(['movies'], getFeaturedMovies)
 
-	if (isLoading) {
-		return <Loading />;
-	  }
+	useEffect(() => {
+		setShowAnimation(true);
+		setTimeout(() => {
+			setShowAnimation(false);
+		}, 1000);
+	}, [favoriteMovies]);
+
+	const { isLoading, isError, data, error } = useQuery(
+		["movies"],
+		getFeaturedMovies
+	);
+
 	return (
 		<>
 			<Navbar />
 			<div className="container mx-auto">
+				{showAnimation && <Loading />}
 				<div className="flex flex-wrap -mx-4">
-					{data && data.map((movie:any) => (
-						<MovieList key={movie.id} movie={movie} />
-					))}
+					{data &&
+						data.map((movie: any) => (
+							<MovieList key={movie.id} movie={movie} />
+						))}
 				</div>
 			</div>
 		</>
